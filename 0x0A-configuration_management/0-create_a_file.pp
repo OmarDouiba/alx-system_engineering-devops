86149f03944d18b81bfa36006245faa6651902f5
DouibaOmar <douibaomar40@gmail.com>
2023-12-22 15:00:41 +0100

    Done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 file { '/tmp/school':
     ensure  => present,
-    mode    => '0774',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
