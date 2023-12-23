423bce9dcc5faa34d417ba658ed7d7c1803bf479
DouibaOmar <douibaomar40@gmail.com>
2023-12-23 10:03:20 +0100

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 #!/usr/bin/pup
 # Install the flask package
+package { 'python3':
+  ensure => '3.8.10',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
+}
+
+package {'Werkzeug':
+  ensure => '2.1.1'
 }
