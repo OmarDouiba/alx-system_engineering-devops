c190b80a0727f728f004bdbd25b0a221acdbbcf5
DouibaOmar <douibaomar40@gmail.com>
2024-01-19 11:46:33 +0100

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,15 @@
 # Install Python 3
+# Install Flask using pip3
+# Install Werkzeug using pip3
+
 package { 'python3':
     ensure  => 3.8.10,
-    provider    => 'pip3',
 }
-# Install Flask using pip3
 package { 'flask':
     ensure      => '2.1.0',
     provider    => 'pip3',
     require     => Package['python3']
 }
-# Install Werkzeug using pip3
 package { 'Werkzeug':
     ensure      => '2.1.1',
     provider    => 'pip3',
